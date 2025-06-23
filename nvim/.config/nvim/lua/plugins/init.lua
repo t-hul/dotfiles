828547b1a7735a5b56858e54cc334c330dead111
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-06-23 13:48:20 +0200

    Added second <cr> in LazyGit keymap and formatting
    when using <leader>lg to open LazyGit there is a query to press <Enter>
    to continue. Get rid of it by adding a second <cr> at the end of the
    command.
    
    Auto-formatter did some formatting changes.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,131 +1,131 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" }, -- uncomment for format on save
-    cmd = { "ConformInfo" },
-    -- keys = {
-    --   {
-    --     -- Customize or remove this keymap to your liking
-    --     "<leader>f",
-    --     function()
-    --       require("conform").format { async = true }
-    --     end,
-    --     mode = "",
-    --     desc = "Format buffer",
-    --   },
-    -- },
-    opts = require "configs.conform",
-    init = function()
-      -- If you want the formatexpr, here is the place to set it
-      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" }, -- uncomment for format on save
+		cmd = { "ConformInfo" },
+		-- keys = {
+		--   {
+		--     -- Customize or remove this keymap to your liking
+		--     "<leader>f",
+		--     function()
+		--       require("conform").format { async = true }
+		--     end,
+		--     mode = "",
+		--     desc = "Format buffer",
+		--   },
+		-- },
+		opts = require("configs.conform"),
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 
-  -- markdown preview
-  -- install without yarn or npm
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    lazy = true,
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    init = function()
-      vim.g.mkdp_theme = "dark"
-    end,
-  },
+	-- markdown preview
+	-- install without yarn or npm
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		lazy = true,
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		init = function()
+			vim.g.mkdp_theme = "dark"
+		end,
+	},
 
-  -- lazygit integration
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
+	-- lazygit integration
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr><cr>", desc = "LazyGit" },
+		},
+	},
 
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui", -- UI for debugging
-      "theHamsta/nvim-dap-virtual-text", -- Virtual text support
-      "jay-babu/mason-nvim-dap.nvim", -- Mason integration for DAP
-    },
-    config = function()
-      local dap = require "dap"
-      local dapui = require "dapui"
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui", -- UI for debugging
+			"theHamsta/nvim-dap-virtual-text", -- Virtual text support
+			"jay-babu/mason-nvim-dap.nvim", -- Mason integration for DAP
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
 
-      -- Automatically open/close dap-ui on debugging
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
+			-- Automatically open/close dap-ui on debugging
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
 
-      -- Setup dap-ui
-      require("dapui").setup()
+			-- Setup dap-ui
+			require("dapui").setup()
 
-      -- Install DAP servers via Mason
-      require("mason-nvim-dap").setup {
-        ensure_installed = { "codelldb", "cppdbg" }, -- Add required debuggers
-      }
-    end,
-  },
+			-- Install DAP servers via Mason
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "codelldb", "cppdbg" }, -- Add required debuggers
+			})
+		end,
+	},
 
-  --  {"nvim-telescope/telescope-symbols.nvim"}
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	--  {"nvim-telescope/telescope-symbols.nvim"}
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
