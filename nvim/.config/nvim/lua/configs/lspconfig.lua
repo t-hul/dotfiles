810263eb112c51a3f739f41045061abba7206fb7
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-07-31 10:19:48 +0200

    Added support of python environments

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,19 +1,20 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 
 -- EXAMPLE
 local servers = { "html", "pylsp", "marksman", "clangd", "texlab" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
 
 -- configuring single server, example: typescript
@@ -22,18 +23,37 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
-require("lspconfig").pylsp.setup {
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          maxLineLength = 88, -- Change this to your preferred limit
-        },
-      },
-    },
-  },
-}
+lspconfig.pylsp.setup({
+	before_init = function(_, config)
+		local venv = os.getenv("VIRTUAL_ENV")
+		if venv then
+			config.settings.python = {
+				pythonPath = venv .. "/bin/python",
+			}
+		else
+			local root = util.root_pattern(".git", "pyproject.toml")(vim.fn.getcwd())
+			if root and vim.fn.isdirectory(root .. "/.venv") == 1 then
+				config.settings.python = {
+					pythonPath = root .. "/.venv/bin/python",
+				}
+			end
+		end
+	end,
+	settings = {
+		pylsp = {
+			-- configurationSources = { "pycodestyle" },
+			plugins = {
+				pycodestyle = {
+					-- enabled = true,
+					maxLineLength = 88, -- Change this to your preferred limit
+				},
+				-- pyls_mypy = { enabled = true },
+				-- pyflakes = { enabled = true },
+			},
+		},
+	},
+})
 
-require("lspconfig").clangd.setup {
-  cmd = { "clangd", "--clang-tidy" },
-}
+lspconfig.clangd.setup({
+	cmd = { "clangd", "--clang-tidy" },
+})
