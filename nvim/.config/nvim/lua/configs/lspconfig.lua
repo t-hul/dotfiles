dc8533b2c5e2c9bb0de1384267288b5587dbab66
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-08-08 09:43:21 +0200

    Excluded jdtls again
    Do not load jdtls here and in ftplugin/java.lua at the same time!
    This would double load the LSP and this one is unconfigured, so I
    removed it again from the list

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require("lspconfig")
 local util = require("lspconfig/util")
 
 -- EXAMPLE
-local servers = { "html", "pylsp", "marksman", "clangd", "texlab" }
+local servers = { "html", "pylsp", "marksman", "clangd", "texlab" } -- , "jdtls"
 local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
