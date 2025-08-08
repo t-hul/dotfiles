e6ea384b184fcd3c086e0deb956af1b3641159bd
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-08-08 09:41:24 +0200

    Added java formatter

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,25 +1,26 @@
 local options = {
-  -- Define your formatters
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "isort", "black" },
-    -- javascript = { "prettierd", "prettier", stop_after_first = true },
-    cpp = { "clang-format" },
-    tex = { "latexindent" },
-    plaintex = { "latexindent" },
-  },
-  -- Set default options
-  default_format_opts = {
-    lsp_format = "fallback",
-  },
-  -- Set up format-on-save
-  format_on_save = { timeout_ms = 1000 },
-  -- Customize formatters
-  formatters = {
-    shfmt = {
-      prepend_args = { "-i", "2" },
-    },
-  },
+	-- Define your formatters
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		-- javascript = { "prettierd", "prettier", stop_after_first = true },
+		cpp = { "clang-format" },
+		tex = { "latexindent" },
+		plaintex = { "latexindent" },
+		java = { "google_java_format" },
+	},
+	-- Set default options
+	default_format_opts = {
+		lsp_format = "fallback",
+	},
+	-- Set up format-on-save
+	format_on_save = { timeout_ms = 1000 },
+	-- Customize formatters
+	formatters = {
+		shfmt = {
+			prepend_args = { "-i", "2" },
+		},
+	},
 }
 
 return options
